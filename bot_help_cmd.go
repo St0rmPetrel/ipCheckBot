994d5b6f07d9217376552900d59f851f20909aad
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2021-08-26 22:30:35 +0300

    Add help msg and screenshot of workflow

--- bot_help_cmd.go
+++ bot_help_cmd.go
@@ -22,11 +22,25 @@ func help(db *gorm.DB, text string, user *tgbotapi.User, chat_id int64) string {
 }
 
 func help_admin() string {
-	msg := "Help admin msg"
+	msg := help_user() + "\n"
+	msg += "---> Admin Help <---\n"
+	msg += `"/admin_new [user_id]" - add to user with <user_id> `
+	msg += `admin permissions` + "\n"
+
+	msg += `"/admin_delete [user_id]" - take away from user with <user_id> `
+	msg += `amdin permissions` + "\n"
+
+	msg += `"/admin_user_history [user_id]" - show all request of user `
+	msg += `with <user_id>` + "\n"
+
+	msg += `"/admin_send_all [msg]" - send <msg> to all familiar to `
+	msg += `bot users` + "\n"
 	return msg
 }
 
 func help_user() string {
-	msg := "Help user msg"
+	msg := "---> User Help <---\n"
+	msg += `"/ip [some_ipV4]" - show info about <some_ipV4>` + "\n"
+	msg += `"/history" - shows all your requested ips` + "\n"
 	return msg
 }
